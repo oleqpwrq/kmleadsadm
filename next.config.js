3bb63e66a69af0036c6d2c2e2b11b400bf5ec82a
Олег <oleqpwrq@Noutbuk-Oleg.local>
2025-05-21 15:55:48 +0300

    feat: добавлена конфигурация для деплоя на GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/kmleadsadm',
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/kmleadsadm' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/kmleadsadm/' : '',
-  trailingSlash: true,
 }
 
 module.exports = nextConfig 
