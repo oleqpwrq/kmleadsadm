a86eef036e3fe0b8ea83cfdbff48bc9a907a50cd
Олег <oleqpwrq@Noutbuk-Oleg.local>
2025-05-21 15:40:53 +0300

    fix: update GitHub Pages configuration

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '/kmleadsadm',
+  basePath: process.env.NODE_ENV === 'production' ? '/kmleadsadm' : '',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig 
